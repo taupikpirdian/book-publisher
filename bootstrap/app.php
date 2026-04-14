83aae3dae10ed8f5be1fa83d00d94d9483214a9b
Taupik Pirdian <pirdiantaupik@gmail.com>
2026-04-15 06:42:18 +0700

    fix: invinite redirect force https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         // Trust all proxy headers for HTTPS detection
-        $middleware->trustProxies(at: '*');
-        
+        // This is essential when behind Cloudflare, nginx, or other reverse proxies
+        $middleware->trustProxies(
+            at: '*',
+            headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | 
+                     \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST | 
+                     \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT | 
+                     \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO | 
+                     \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
         // Force HTTPS in production
         $middleware->appendToGroup('web', [
             \App\Http\Middleware\ForceHttps::class,
